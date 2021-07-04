052ec9f95ad9d58fedaf1218e75b0ab9f88ee194
jellappi324 <jellappi324@naver.com>
2021-07-04 12:28:29 +0000

    add inference code

--- api/app.py
+++ api/app.py
@@ -1,11 +1,20 @@
 import jwt
 import bcrypt
+import re
+import nltk
+import pickle
 
-from flask      import Flask, request, jsonify, current_app, Response, g
-from flask.json import JSONEncoder
-from sqlalchemy import create_engine, text
-from datetime   import datetime, timedelta
-from functools  import wraps
+from flask       import Flask, request, jsonify, current_app, Response, g
+from flask.json  import JSONEncoder
+from sqlalchemy  import create_engine, text
+from datetime    import datetime, timedelta
+from functools   import wraps
+from nltk.corpus import stopwords
+from konlpy.tag  import Kkma
+
+import pandas  as pd
+import xgboost as xgb
+
 
 ## Default JSON encoder는 set를 JSON으로 변환할 수 없다.
 ## 그럼으로 커스텀 엔코더를 작성해서 set을 list로 변환하여
@@ -22,7 +31,7 @@ def get_user(user_id):
         SELECT 
             id,
             name,
-            email,
+            email
         FROM users
         WHERE id = :user_id
     """), {
@@ -48,53 +57,6 @@ def insert_user(user):
         )
     """), user).lastrowid
 
-# def insert_tweet(user_tweet):
-#     return current_app.database.execute(text("""
-#         INSERT INTO tweets (
-#             user_id,
-#             tweet
-#         ) VALUES (
-#             :id,
-#             :tweet
-#         )
-#     """), user_tweet).rowcount
-
-# def insert_follow(user_follow):
-#     return current_app.database.execute(text("""
-#         INSERT INTO users_follow_list (
-#             user_id,
-#             follow_user_id
-#         ) VALUES (
-#             :id,
-#             :follow
-#         )
-#     """), user_follow).rowcount
-
-# def insert_unfollow(user_unfollow):
-#     return current_app.database.execute(text("""
-#         DELETE FROM users_follow_list
-#         WHERE user_id = :id
-#         AND follow_user_id = :unfollow
-#     """), user_unfollow).rowcount
-
-# def get_timeline(user_id):
-#     timeline = current_app.database.execute(text("""
-#         SELECT 
-#             t.user_id,
-#             t.tweet
-#         FROM tweets t
-#         LEFT JOIN users_follow_list ufl ON ufl.user_id = :user_id
-#         WHERE t.user_id = :user_id 
-#         OR t.user_id = ufl.follow_user_id
-#     """), {
-#         'user_id' : user_id 
-#     }).fetchall()
-
-#     return [{
-#         'user_id' : tweet['user_id'],
-#         'tweet'   : tweet['tweet']
-#     } for tweet in timeline]
-
 def get_user_id_and_password(email):
     row = current_app.database.execute(text("""    
         SELECT
@@ -108,6 +70,42 @@ def get_user_id_and_password(email):
         'id'              : row['id'],
         'hashed_password' : row['hashed_password']
     } if row else None
+
+def extract_kor(string):
+    
+    kkma = Kkma()
+    
+    text_data = [] 
+    
+    ex_pos = kkma.pos(string)
+    
+    for (text, tclass) in ex_pos : # ('형태소', 'NNG') 
+        if tclass == 'NNG' or tclass == 'NNP' or tclass == 'NP' : 
+            text_data.append(text)  
+            
+    return text_data
+
+
+def extract_eng(string):
+    
+    wlem = nltk.WordNetLemmatizer()
+    
+    cleaned_content = re.sub(r'[^\.\?\!\w\d\s]', '', string)
+    
+    word_tokens = nltk.word_tokenize(cleaned_content)
+    tokens_pos = nltk.pos_tag(word_tokens)
+    
+    NN_words = []
+    for word, pos in tokens_pos:
+        if 'NN' in pos:
+            NN_words.append(word.lower())
+            
+    lemmatized_words = []
+    for word in NN_words:
+        new_word = wlem.lemmatize(word)
+        lemmatized_words.append(new_word)
+        
+    return lemmatized_words
 
 #########################################################
 #       Decorators
@@ -183,47 +181,62 @@ def create_app(test_config = None):
             })
         else:
             return '', 401
-
-    @app.route('/tweet', methods=['POST'])
+    
+    @app.route('/infer', methods=['POST'])
     @login_required
-    def tweet():
-        user_tweet       = request.json
-        user_tweet['id'] = g.user_id
-        tweet            = user_tweet['tweet']
+    def infer():
+        jd       = request.json
+        jd['id'] = g.user_id
+        
+        # extract
+        if (jd['position'] == None) & (jd['main_tasks'] == None):
+            print("No input data. At least one of position and main_tasks is needed.")
 
-        if len(tweet) > 300:
-            return '300자를 초과했습니다', 400
+        elif jd['position'] != None:
+            ex = extract_kor(jd['position'])
 
-        insert_tweet(user_tweet)
+            if len(ex) == 0:
+                ex = extract_eng(jd['position'])
 
-        return '', 200
+        else:
+            ex = extract_kor(jd['main_tasks'])
 
-    @app.route('/follow', methods=['POST'])
-    @login_required
-    def follow():
-        payload       = request.json
-        payload['id'] = g.user_id
+            if len(ex) == 0:
+                ex = extract_eng(jd['main_tasks'])
+                
+                
+        # load data        
+        columns = list(pd.read_csv('/home/ubuntu/data/preprocessed_data.csv', nrows=0).columns)
+        columns.remove('category')
+        
+        model = pickle.load(open('/home/ubuntu/model/xgb-model', 'rb'))
+        
+        encoding = pd.read_csv('/home/ubuntu/data/category_encoding.csv', sep='\t', index_col=0)
+        
+        
+        # request preprocessing
+        df = pd.DataFrame(columns=columns)
+        df.loc[len(df)] = 0
 
-        insert_follow(payload) 
+        for col in columns:
+            df[col] = df[col].astype('int')
 
-        return '', 200
+        for w in ex:
+            if w in df.columns:
+                df.loc[0, w] = 1
 
-    @app.route('/unfollow', methods=['POST'])
-    @login_required
-    def unfollow():
-        payload       = request.json
-        payload['id'] = g.user_id
+        dtest = xgb.DMatrix(data=df)
+        
+        
+        # inference
+        predicted_code = int(model.predict(dtest)[0])
+        infer_result = encoding.loc[predicted_code, 'category']
 
-        insert_unfollow(payload)
-
-        return '', 200
-
-    @app.route('/timeline/<int:user_id>', methods=['GET'])
-    def timeline(user_id):
+        
         return jsonify({
-            'user_id'  : user_id,
-            'timeline' : get_timeline(user_id)
+            'category' : infer_result
         })
+    
 
     return app
 
--- api/config.py
+++ api/config.py
@@ -1,9 +1,12 @@
 db = {
     'user'     : 'root',
-    'password' : 'test1234',
+    'password' : 'Test1234##',
     'host'     : 'localhost',
     'port'     : 3306,
     'database' : 'db'
 }
+
+JWT_SECRET_KEY        = 'SOME_SUPER_SECRET_KEY'
+JWT_EXP_DELTA_SECONDS = 7 * 24 * 60 * 60
 
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
